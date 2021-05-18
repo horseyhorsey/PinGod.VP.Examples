fe4516020e1407020c868496c38a374d7a6ece1a
Dave <dub.cyclone@gmail.com>
2021-05-18 18:41:22 +0100

    Attract mode now cycles from `PUSH START` to displaying high scores.

--- src/BasicGame/BasicGameGodot/modes/common/attract/Attract.cs
+++ src/BasicGame/BasicGameGodot/modes/common/attract/Attract.cs
@@ -3,8 +3,39 @@ using static Godot.GD;
 
 public class Attract : Node2D
 {
+	int _currentScene = 0;
+
+	public override void _Input(InputEvent @event)
+	{
+		//Start button. See PinGod.vbs for Standard switches
+		if (@event.IsActionPressed("sw" + GameGlobals.StartSwitchNum))
+		{
+			Print("attract: starting game");
+			(GetNode("/root/GameGlobals") as GameGlobals)?.StartGame();
+		}
+	}
+
 	public override void _Ready()
 	{
 		Print("Attract loaded");		
 	}
+	
+	private void _on_Timer_timeout()
+	{
+		_currentScene++;
+		if (_currentScene > 0)
+		{
+			(GetNode("CanvasLayer/TextLayerControl") as TextLayer).Visible = false;
+			(GetNode("CanvasLayer/HighScores") as HighScores).Visible = true;
+			_currentScene = -1;
+		}			
+		else
+		{
+			(GetNode("CanvasLayer/TextLayerControl") as TextLayer).Visible = true;
+			(GetNode("CanvasLayer/HighScores") as HighScores).Visible = false;
+		}
+	}
 }
+
+
+
