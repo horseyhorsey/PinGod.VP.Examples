4cb6883f001d8b8229d53e4a7fb03ec08fcaf819
Dave <dub.cyclone@gmail.com>
2021-05-19 17:52:50 +0100

    Bonus: Changed from canvasLayer to Control. Added player checks on init and `StartBonusDisplay` for the parent to use.

--- src/BasicGame/BasicGameGodot/modes/common/bonus/Bonus.cs
+++ src/BasicGame/BasicGameGodot/modes/common/bonus/Bonus.cs
@@ -4,9 +4,9 @@ using static Godot.GD;
 
 /// <summary>
 /// A bonus layer / mode. Display at end of ball. Bonus.tscn scene <para/>
-/// Sends <see cref="GameGlobals.BonusEnded"/> signal on <see cref="_display_for_seconds"/> timeout
+/// Sends <see cref="PinGodGame.BonusEnded"/> signal on <see cref="_display_for_seconds"/> timeout
 /// </summary>
-public class Bonus : CanvasLayer
+public class Bonus : Control
 {
 	private Timer timer;
 
@@ -14,15 +14,22 @@ public class Bonus : CanvasLayer
 
 	public override void _Ready()
 	{
-		GameGlobals.Player.Points += GameGlobals.Player.Bonus;
+		if(PinGodGame.Player != null)
+		{
+			PinGodGame.Player.Points += PinGodGame.Player.Bonus;
+		}		
 
 		timer = GetNode("Timer") as Timer;
 		if (!timer.IsStopped())
-			timer.Stop();
+			timer.Stop();		
+	}
 
+	public void StartBonusDisplay()
+	{
 		var label = GetNode("Label") as Label;
 		label.Text = SetBonusText();
 		timer.Start(_display_for_seconds);
+		Visible = true;
 	}
 
 	/// <summary>
@@ -33,14 +40,18 @@ public class Bonus : CanvasLayer
 	{		
 		var txt = "END OF BALL" + System.Environment.NewLine;
 		txt += "BONUS" + System.Environment.NewLine;
-		txt += GameGlobals.Player.Bonus.ToString("N0");		
+		txt += PinGodGame.Player?.Bonus.ToString("N0");		
 		return txt;
 	}
 
+	/// <summary>
+	/// Bonus has times out. Hide the display and send <see cref="PinGodGame.BonusEnded"/>
+	/// </summary>
 	private void _on_Timer_timeout()
 	{
-		Print("bonus: sending BonusEnded");
-		GetNode("/root/GameGlobals").EmitSignal("BonusEnded");		
-		this.QueueFree();
+		Print("bonus: BonusEnded");
+		timer.Stop();
+		this.Visible = false;
+		GetNode("/root/PinGodGame").EmitSignal("BonusEnded");	
 	}
 }
