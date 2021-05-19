abb8eeae453768390fc6d433e8a19b09f005934f
Dave <dub.cyclone@gmail.com>
2021-05-19 17:31:51 +0100

    Added `Switch` with wrappers around the input actions.

---
+++ src/BasicGame/BasicGameGodot/models/Switch.cs
@@ -0,0 +1,31 @@
+﻿using Godot;
+
+public class Switch
+{
+    public Switch(byte num) { this.Num = num; }
+    public Switch(byte num, BallSearchSignalOption ballSearch) { this.Num = num; this.BallSearch = ballSearch; }
+    public byte Num { get; set; }
+    public BallSearchSignalOption BallSearch { get; set; }
+    /// <summary>
+    /// Checks the current input event. IsActionPressed(sw+num)
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public bool IsOn(InputEvent input) => input.IsActionPressed(ToString());
+    /// <summary>
+    /// Checks the current input event. IsActionReleased(sw+num)
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public bool IsOff(InputEvent input) => input.IsActionReleased(ToString());
+    /// <summary>
+    /// Checks if On/Off
+    /// </summary>
+    /// <returns></returns>
+    public bool IsOn() => Input.IsActionPressed(ToString());
+    /// <summary>
+    /// The action. sw+SwitchNum
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString() => "sw" + Num;
+}
