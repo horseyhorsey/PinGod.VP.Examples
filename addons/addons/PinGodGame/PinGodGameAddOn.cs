bc4ffea3e884ec367c10006f596f33d536271e59
Dave <dub.cyclone@gmail.com>
2021-05-31 22:10:26 +0100

    Fixed not able to export. Addons block export if pre processor isn't defined.

--- addons/addons/PinGodGame/PinGodGameAddOn.cs
+++ addons/addons/PinGodGame/PinGodGameAddOn.cs
@@ -1,15 +1,18 @@
-﻿using Godot;
+#if TOOLS
+using Godot;
 using System;
 
+[Tool]
 public class PinGodGameAddOn : EditorPlugin
 {
-    public override void _EnterTree()
-    {
-        base._EnterTree();
-    }
+	public override void _EnterTree()
+	{
+		base._EnterTree();
+	}
 
-    public override void _ExitTree()
-    {
-        base._ExitTree();
-    }
+	public override void _ExitTree()
+	{
+		base._ExitTree();
+	}
 }
+#endif
